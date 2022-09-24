5b08d87e5f7a24a5e18377df8c9387e4dce50973
PercG59 <114250706+PercG59@users.noreply.github.com>
2022-09-24 15:27:01 -0400

    Add files via upload

---
+++ Altis_Life.Altis/core/custom/Medusa_Handler.hpp
@@ -0,0 +1,6 @@
+F(TON_fnc_firstjoin,SERVER)
+F(TON_fnc_fetchexperience,SERVER)
+F(TON_fnc_saveexperienceserver,SERVER)
+F(TON_fnc_firstexperience,SERVER)
+F(life_fnc_firstjoin,CLIENT)
+F(life_fnc_experienceReceived,CLIENT)
